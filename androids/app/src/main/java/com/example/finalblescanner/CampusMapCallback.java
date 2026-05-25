eee9b1e9c16def08a7ebc91e9eb73b34c27d8a6d
KyeongTaek <tommyholic@naver.com>
2026-05-26 03:44:27 +0900

    feat: add marker on map

--- androids/app/src/main/java/com/example/finalblescanner/CampusMapCallback.java
+++ androids/app/src/main/java/com/example/finalblescanner/CampusMapCallback.java
@@ -1,10 +1,30 @@
 package com.example.finalblescanner;
 
+import android.graphics.Color;
+import android.util.Log;
+import android.widget.TextView;
+
 import com.kakao.vectormap.KakaoMap;
 import com.kakao.vectormap.LatLng;
 import com.kakao.vectormap.KakaoMapReadyCallback;
 import com.kakao.vectormap.MapLifeCycleCallback;
+import com.kakao.vectormap.label.Label;
+import com.kakao.vectormap.label.LabelOptions;
+import com.kakao.vectormap.label.LabelStyle;
+import com.kakao.vectormap.label.LabelStyles;
+import com.kakao.vectormap.label.LabelTextBuilder;
+
+import java.util.List;
+import java.util.Map;
+
 public class CampusMapCallback {
+    List<Map<String, Object>> locations;
+    Map<String, Object> responses;
+
+    public CampusMapCallback(List<Map<String, Object>> loc, Map<String, Object> fakeSensorResponse) {
+        locations = loc;
+        responses = fakeSensorResponse;
+    }
     public final MapLifeCycleCallback lifeCycleCallback = new MapLifeCycleCallback() {
         @Override
         public void onMapDestroy() {
@@ -21,12 +41,61 @@ public class CampusMapCallback {
         @Override
         public void onMapReady(KakaoMap kakaoMap) {
             // 인증 후 API 가 정상적으로 실행될 때 호출됨
+            LabelStyles styles = LabelStyles.from("myStyleId",
+                    LabelStyle.from(R.drawable.red_marker).setZoomLevel(8),
+                    LabelStyle.from(R.drawable.blue_marker).setZoomLevel(11),
+                    LabelStyle.from(R.drawable.blue_marker).setTextStyles(32, Color.BLACK, 1, Color.GRAY).setZoomLevel(15));
+
+            styles = kakaoMap.getLabelManager().addLabelStyles(styles);
+            LabelStyles orange_style = LabelStyles.from("orange_style_id",
+                    LabelStyle.from(R.drawable.orange_marker).setTextStyles(32, Color.BLACK, 1, Color.GRAY));
+            LabelStyles green_style = LabelStyles.from("green_style_id",
+                    LabelStyle.from(R.drawable.green_marker).setTextStyles(32, Color.BLACK, 1, Color.GRAY));
+            LabelStyles blue_style = LabelStyles.from("blue_style_id",
+                    LabelStyle.from(R.drawable.blue_marker).setTextStyles(32, Color.BLACK, 1, Color.GRAY));
+            LabelStyles gray_style = LabelStyles.from("gray_style_id",
+                    LabelStyle.from(R.drawable.gray_marker).setTextStyles(32, Color.BLACK, 1, Color.GRAY));
+
+            for (Map<String, Object> location : locations) {
+                Map<String, Object> building_data = (Map)responses.get("data");
+                int key = 0;
+                for (Map<String, Object> m : (List<Map<String, Object>>)building_data.get("buildings")) {
+                    if ((location.get("building")).toString().equals(m.get("building_name"))) {
+                        break;
+                    }
+                    else {
+                        key = key + 1;
+                    }
+                }
+
+                String operating_status = ((Map)((List)building_data.get("buildings")).get(key)).get("operating_status").toString();
+                switch (operating_status) {
+                    case "COOLING_REQUIRED":
+                        styles = kakaoMap.getLabelManager().addLabelStyles(orange_style);
+                        break;
+                    case "POWER_SAVING_REQUIRED":
+                        styles = kakaoMap.getLabelManager().addLabelStyles(green_style);
+                        break;
+                    case "HEATING_REQUIRED":
+                        styles = kakaoMap.getLabelManager().addLabelStyles(blue_style);
+                        break;
+                    default:
+                        styles = kakaoMap.getLabelManager().addLabelStyles(gray_style);
+                        break;
+                }
+
+                Label label = kakaoMap.getLabelManager().getLayer().addLabel(LabelOptions.from(setPosition((Double)location.get("lat"), (Double)location.get("lon")))
+                        .setStyles(styles));
+            }
         }
 
         @Override
         public LatLng getPosition() {
             // 지도 시작 시 위치 좌표를 설정
             return LatLng.from(36.629000, 127.457000);
+        }
+        public LatLng setPosition(double lat, double lon) {
+            return LatLng.from(lat, lon);
         }
     };
 }
--- androids/app/src/main/java/com/example/finalblescanner/MainActivity.java
+++ androids/app/src/main/java/com/example/finalblescanner/MainActivity.java
@@ -11,9 +11,58 @@ import com.kakao.vectormap.KakaoMapSdk;
 import com.kakao.vectormap.MapView;
 
 import android.util.Log;
+import android.widget.TextView;
+
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     public MapView mapView;
+
+    List<Map<String, Object>> building_loc = List.of(
+            Map.of(
+                    "building", "CBNU스포츠센터",
+                    "lat", 36.62731552,
+                    "lon", 127.4608165
+            ),
+            Map.of(
+                    "building", "중앙도서관",
+                    "lat", 36.62848293,
+                    "lon", 127.4574258
+            )
+    );
+    Map<String, Object> fakeSensorResponse = Map.of(
+            "statusCode", 200,
+            "message", "request success",
+            "data", Map.of(
+                    "campus_humidity", 44.5,
+                    "campus_aqi", 2,
+                    "buildings", List.of(
+                            Map.of(
+                                    "building_name", "CBNU스포츠센터",
+                                    "building_ext_temp", 20.6,
+                                    "building_ext_co2", 600,
+                                    "operating_status", "POWER_SAVING_REQUIRED",
+                                    "recommendation_msg", "현재 냉난방이 필요 없는 최적의 외부 기후입니다. 적극적인 자연환기를 전개하고, 모든 공조 설비를 절전/송풍 모드로 전환하여 에너지를 절약하세요."
+                            ),
+                            Map.of(
+                                    "building_name", "중앙도서관",
+                                    "building_ext_temp", 26.5,
+                                    "building_ext_co2", 480,
+                                    "operating_status", "COOLING_REQUIRED",
+                                    "recommendation_msg", "환기하기 좋은 날씨네요. 그러나 외기 온도가 매우 가파르게 상승하고 있어, 창문을 열기보다는 냉방을 유지하는 것이 에너지를 절약하는 길입니다."
+                            )
+                    )
+            )
+    );
+
+    public TextView buildingView;
+    public TextView tempView;
+    public TextView co2View;
+    public TextView statusView;
+    public TextView ventilationView;
+    public TextView tagView;
+    public TextView operationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +73,27 @@ public class MainActivity extends AppCompatActivity {
         KakaoMapSdk.init(this, BuildConfig.API_KEY);
         mapView = findViewById(R.id.map_view);
 
-        CampusMapCallback campusMapCallback = new CampusMapCallback();
+        CampusMapCallback campusMapCallback = new CampusMapCallback(building_loc, fakeSensorResponse);
         mapView.start(campusMapCallback.lifeCycleCallback, campusMapCallback.readyCallback);
 
         // onCreate 내부 또는 적절한 위치에 추가
         String keyHash = KakaoMapSdk.INSTANCE.getHashKey();
         Log.d("KakaoKeyHash", "내 키 해시값: " + keyHash);
+
+//        Map<String, Object> building_data = (Map)fakeSensorResponse.get("data");
+//        buildingView = findViewById(R.id.buildingName_TextView);
+//        buildingView.setText(((Map)((List)building_data.get("buildings")).get(0)).get("building_name").toString());
+//        tempView = findViewById(R.id.temp_TextView);
+//        tempView.setText(((Map)((List)building_data.get("buildings")).get(0)).get("building_ext_temp").toString());
+//        co2View = findViewById(R.id.co2_TextView);
+//        co2View.setText(((Map)((List)building_data.get("buildings")).get(0)).get("building_ext_co2").toString());
+//        statusView = findViewById(R.id.status_TextView);
+//        statusView.setText(((Map)((List)building_data.get("buildings")).get(0)).get("operating_status").toString());
+//        ventilationView = findViewById(R.id.ventilation_TextView);
+//        ventilationView.setText(building_data.get("campus_humidity").toString());
+//        operationView = findViewById(R.id.operation_TextView);
+//        operationView.setText(((Map)((List)building_data.get("buildings")).get(0)).get("recommendation_msg").toString());
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
